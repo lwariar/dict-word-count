5c5ba4fadbd4a0ac5009d6af5eaf66748d883cad
vagrant <vagrant@vagrant.vm>
2020-10-05 22:03:31 +0000

    sorting done

--- wordcount.py
+++ wordcount.py
@@ -1,9 +1,10 @@
 # put your code here.
 import sys
+
 def get_word_count():
     # read the filename from the command line
     text = open(sys.argv[1])
-
+    
     word_counts = {}
     for line in text:
         # remove the trailing spaces and new line chars
@@ -18,10 +19,10 @@ def get_word_count():
             word = word.lower()
             word_counts[word] = word_counts.get(word, 0) + 1
 
-    sorted_word_counts = sorted(word_counts.items(), key=lambda x: x[1])
+    sorted_word_counts = reversed(sorted(word_counts.items(), key=lambda x: x[1]))
     for i in sorted_word_counts:
     	print(i[0], i[1])        
     
     return word_counts
-
+    
 get_word_count()
